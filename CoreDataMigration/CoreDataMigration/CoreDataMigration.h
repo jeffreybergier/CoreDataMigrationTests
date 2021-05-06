81504186b910fc827c5ce822fa1d0c5c58806f0d
jeffreybergier <jeffburg@jeffburg.com>
2021-05-06 20:15:24 +0900

    initial commit

---
+++ CoreDataMigration/CoreDataMigration/CoreDataMigration.h
@@ -0,0 +1,18 @@
+//
+//  CoreDataMigration.h
+//  CoreDataMigration
+//
+//  Created by Jeffrey Bergier on 2021/05/06.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreDataMigration.
+FOUNDATION_EXPORT double CoreDataMigrationVersionNumber;
+
+//! Project version string for CoreDataMigration.
+FOUNDATION_EXPORT const unsigned char CoreDataMigrationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataMigration/PublicHeader.h>
+
+
