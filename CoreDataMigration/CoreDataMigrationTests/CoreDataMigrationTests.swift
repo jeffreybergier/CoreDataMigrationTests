f24c10ba48e5b031deccb4e10776687e4aed4906
jeffreybergier <jeffburg@jeffburg.com>
2021-05-07 20:56:56 +0900

    fixed tests

--- CoreDataMigration/CoreDataMigrationTests/CoreDataMigrationTests.swift
+++ CoreDataMigration/CoreDataMigrationTests/CoreDataMigrationTests.swift
@@ -9,18 +9,19 @@ import XCTest
 import CoreData
 import CoreDataMigration
 
-let MyBundle = Bundle(for: PlainCoreDataMigrationTests.self)
+let MyBundle = Bundle(for: AbstractCoreDataMigrationTests.self)
 let Example_v1_0_0 = MyBundle.url(forResource: "Example_v1.0.0", withExtension: ".sqlite")!
 let Example_v1_1_0 = MyBundle.url(forResource: "Example_v1.1.0", withExtension: ".sqlite")!
 let Example_v1_1_2 = MyBundle.url(forResource: "Example_v1.1.2", withExtension: ".sqlite")!
 let Example_v1_2_0 = MyBundle.url(forResource: "Example_v1.2.0", withExtension: ".sqlite")!
 let Example_v2_0_0 = MyBundle.url(forResource: "Example_v2.0.0", withExtension: ".sqlite")!
+let KeyToFetch = "v2_0_0_name"
 
-class PlainCoreDataMigrationTests: XCTestCase {
+class AbstractCoreDataMigrationTests: XCTestCase {
     
     let randomStoreURL = URL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
                             .appendingPathComponent(UUID().uuidString, isDirectory: true)
-    var containerType: NSPersistentContainer.Type { PlainContainer.self }
+    var containerType: NSPersistentContainer.Type { fatalError() }
     var exampleStoreDestination: URL { self.randomStoreURL.appendingPathComponent("Migration.sqlite") }
     
     override func setUpWithError() throws {
@@ -36,47 +37,51 @@ class PlainCoreDataMigrationTests: XCTestCase {
     }
 
     func testMigraion_v1_0_0() throws {
-        try FileManager.default.moveItem(at: Example_v1_0_0, to: self.exampleStoreDestination)
+        try FileManager.default.copyItem(at: Example_v1_0_0, to: self.exampleStoreDestination)
         let stack = try Stack(self.containerType)
         let results = try stack.fetchAll()
         XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results[0].value(forKey: "v2_0_0_name") as? String, "A Known Value")
+        XCTAssertEqual(results[0].value(forKey: KeyToFetch) as? String, "A Known Value")
     }
     
     func testMigration_v1_1_0() throws {
-        try FileManager.default.moveItem(at: Example_v1_1_0, to: self.exampleStoreDestination)
+        try FileManager.default.copyItem(at: Example_v1_1_0, to: self.exampleStoreDestination)
         let stack = try Stack(self.containerType)
         let results = try stack.fetchAll()
         XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results[0].value(forKey: "v2_0_0_name") as? String, "A Known Value")
+        XCTAssertEqual(results[0].value(forKey: KeyToFetch) as? String, "A Known Value")
     }
     
     func testMigration_v1_1_2() throws {
-        try FileManager.default.moveItem(at: Example_v1_1_2, to: self.exampleStoreDestination)
+        try FileManager.default.copyItem(at: Example_v1_1_2, to: self.exampleStoreDestination)
         let stack = try Stack(self.containerType)
         let results = try stack.fetchAll()
         XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results[0].value(forKey: "v2_0_0_name") as? String, "A Known Value")
+        XCTAssertEqual(results[0].value(forKey: KeyToFetch) as? String, "A Known Value")
     }
     
     func testMigration_v1_2_0() throws {
-        try FileManager.default.moveItem(at: Example_v1_2_0, to: self.exampleStoreDestination)
+        try FileManager.default.copyItem(at: Example_v1_2_0, to: self.exampleStoreDestination)
         let stack = try Stack(self.containerType)
         let results = try stack.fetchAll()
         XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results[0].value(forKey: "v2_0_0_name") as? String, "A Known Value")
+        XCTAssertEqual(results[0].value(forKey: KeyToFetch) as? String, "A Known Value")
     }
     
     func testMigration_v2_0_0() throws {
-        try FileManager.default.moveItem(at: Example_v2_0_0, to: self.exampleStoreDestination)
+        try FileManager.default.copyItem(at: Example_v2_0_0, to: self.exampleStoreDestination)
         let stack = try Stack(self.containerType)
         let results = try stack.fetchAll()
         XCTAssertEqual(results.count, 1)
-        XCTAssertEqual(results[0].value(forKey: "v2_0_0_name") as? String, "A Known Value")
+        XCTAssertEqual(results[0].value(forKey: KeyToFetch) as? String, "A Known Value")
     }
 
 }
 
-class CloudCoreDataMigrationTests: PlainCoreDataMigrationTests {
+class PlainCoreDataMigrationTests: AbstractCoreDataMigrationTests {
+    override var containerType: NSPersistentContainer.Type { PlainContainer.self }
+}
+
+class CloudCoreDataMigrationTests: AbstractCoreDataMigrationTests {
     override var containerType: NSPersistentContainer.Type { CloudContainer.self }
 }
